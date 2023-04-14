7eb57e9401afa300834f84c0ef03d3774ffc6003
Danil <shwedx@Yandex.ru>
2023-04-14 09:46:13 +0300

    done

---
+++ 2 task/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+//M = 4; N = 8. -> 30
+
+System.Console.Write("Введите первое число:");
+int number1 = int.Parse(Console.ReadLine());
+
+System.Console.Write("Введите второе число:");
+int number2 = int.Parse(Console.ReadLine());
+
+int getsum(int number1,int number2)
+{
+    if(number2 == number1)
+    {
+        return number2;
+    }
+    return number2 + getsum(number1,number2-1);
+    
+    
+
+}
+
+int result = getsum(number1,number2);
+
+System.Console.Write(result);
---
+++ 4 task/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача со звездочкой Найти второй максимум в массиве.
+//4, 4, 3, 0, 2 => 3
+ int FindSecondMax(int[] arr)
+    {
+        int max1 = arr[0];
+        int max2 = arr[0];
+
+        foreach (int num in arr)
+        {
+            if (num > max1)
+            {
+                max2 = max1;
+                max1 = num;
+            }
+            else if (num > max2)
+            {
+                max2 = num;
+            }
+        }
+
+        return max2;
+    }
+
+    
+int[] arr = { 5, 2, 8, 7, 3, 9 };
+int secondMax = FindSecondMax(arr);
+Console.WriteLine(secondMax);
+
+
+
